6f6ed59373f04d08f6d9f97fa4aef039935ef5ba
Chandra Munukutla <csmunuku@gmail.com>
2018-01-31 11:45:18 -0800

    few changes

--- bash/wl_version.sh
+++ bash/wl_version.sh
@@ -7,7 +7,6 @@ MY_DOMAIN_HOME="${DOMAIN_HOME}"
 MY_WL_HOME="${WL_HOME}"
 MY_MW_HOME="${MW_HOME}"
 MY_ADMIN_SERVER_LOG="$MY_DOMAIN_HOME/servers/AdminServer/admin/AdminServer.log"
-MY_JAVA="java"
 
 if [[ -d ${MY_MW_HOME} && -f ${MY_MW_HOME}/registry.xml ]]; then
    grep "component name=\"WebLogic Server\"" ${MY_MW_HOME}/registry.xml | grep version
@@ -17,5 +16,6 @@ if [[ -f ${MY_ADMIN_SERVER_LOG} ]]; then
 fi
 if [[ -d ${MY_WL_HOME}/server/lib ]]; then
    cd ${MY_WL_HOME}/server/lib
+   command -v java >/dev/null 2>&1 || { echo "java NOT FOUND!!.  Aborting." >&2; exit 1; }
    java -cp weblogic.jar weblogic.version
 fi
