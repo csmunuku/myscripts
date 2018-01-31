ccb80fe314e1300078149f67a920a9411c847a5a
Chandra Munukutla <csmunuku@gmail.com>
2018-01-31 11:26:36 -0800

    wl version and new changeFile

---
+++ bash/wl_version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Find WebLogic Server Version
+###################################
+# Define current env specific variables for WebLogic
+
+MY_DOMAIN_HOME="${DOMAIN_HOME}"
+MY_WL_HOME="${WL_HOME}"
+MY_MW_HOME="${MW_HOME}"
+MY_ADMIN_SERVER_LOG="$MY_DOMAIN_HOME/servers/AdminServer/admin/AdminServer.log"
+MY_JAVA="java"
+
+if [[ -d ${MY_MW_HOME} && -f ${MY_MW_HOME}/registry.xml ]]; then
+   grep "component name=\"WebLogic Server\"" ${MY_MW_HOME}/registry.xml | grep version
+fi
+if [[ -f ${MY_ADMIN_SERVER_LOG} ]]; then
+   grep "WebLogic Server" ${MY_ADMIN_SERVER_LOG} | awk '{print $3}'
+fi
+if [[ -d ${MY_WL_HOME}/server/lib ]]; then
+   cd ${MY_WL_HOME}/server/lib
+   java -cp weblogic.jar weblogic.version
+fi
